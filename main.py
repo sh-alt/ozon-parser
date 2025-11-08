e8309d8f06d44a1f4dabc88e712e1314a1545281
Sokolov <sokol.89@list.ru>
2025-11-08 23:36:15 +0300

    fix: use PORT env variable in CMD

--- main.py
+++ main.py
@@ -218,4 +218,6 @@ async def parse_product(request: ParseRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
